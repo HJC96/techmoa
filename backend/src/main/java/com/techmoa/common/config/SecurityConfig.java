b348dca917321996459f56d4cb0cf256933e0186
HJC96 <gkswlcjs2@naver.com>
2026-02-21 14:52:39 +0900

    Add HTTPS support with Nginx and configure SSL certificates

--- backend/src/main/java/com/techmoa/common/config/SecurityConfig.java
+++ backend/src/main/java/com/techmoa/common/config/SecurityConfig.java
@@ -41,7 +41,8 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(List.of(
                 "http://43.202.208.107",
                 "http://localhost:5173",
-                "http://127.0.0.1:5173"
+                "http://127.0.0.1:5173",
+                "https://techmoa.today "
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
